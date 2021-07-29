4e2d967711a5c8e2899ef47645c14f1db6802652
Joao Lima <joaoplima@ua.pt>
2021-07-29 19:15:00 +0200

    Creating CRU

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Password-Manager
+
+This is a simple password manager, created in Flash and SQLAlchemy to store the data.
+
+## ScreenShot 
+![alt text](img\app.png)
+
+## Install
+1. Clone the repo
+2. Create a virtual env
+3. pip install from requirements
+4. Run app.py
--- app.py
+++ app.py
@@ -1,30 +1,90 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-import sqlalchemy
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
+# Initialize app
 app = Flask(__name__)
+# Create sesssion & set permanet session to 5 min
 app.secret_key = "key"
+app.permanent_session_lifetime = timedelta(minutes=5)
+# Config Database, Table records
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///records.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create a Database
+db = SQLAlchemy(app)
+
+# Define a class to represent the records
+class Records(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    application = db.Column("application", db.String(100))
+    password = db.Column("password", db.String(100))
+
+    def __init__(self, application, password):
+        self.application = application   
+        self.password = password   
+
 
 @app.route('/')
-@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('results.html', all_records=Records.query.all())
 
 @app.route('/new', methods=['POST', 'GET'])
 def new():
     if request.method == 'POST':
-        session['application'] = request.form.get('application')
-        return redirect(url_for('application'))
+        application = request.form["application"]
+        password = request.form["password"]
+        rec = Records(application, password)
+
+        # Save rec in the Database
+        db.session.add(rec)
+        db.session.commit()
+
+        return redirect(url_for('index'))
     else:
         return render_template('new.html')
 
-@app.route('/application')
-def application():
-    if "application" in session:
-        application = session["application"]
-        return f'<h1>{application}</h1>'
-    else: 
-        return redirect(url_from('index.html'))
-    
+@app.route('/edit/<r>', methods=['POST', 'GET'])
+def edit(r):
+    # ID from the record
+    record = Records.query.get(r) 
+    if request.method == 'POST':
+        # Get the date from the form 
+        application = request.form["application"]
+        password = request.form["password"]
+
+        # Update the data
+        record.application = application
+        record.password = password
+        
+        # Save changes in the Database
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    else:              
+        return render_template('edit.html', record=record)
+
+   
+@app.route('/search')
+def search():
+    word = request.args.get('search')
+    print(f'---------------------------------------{word}')
+
+
+
+    if word == '':     
+        return render_template('search.html', records=Records.query.all())
+
+    else:        
+        return render_template('search.html', records=Records.query.filter(Records.application.startswith(word)).all())
+
+
+
+    #search_query = Records.query.filter(Records.application.like(key))
+    #return render_template('search.html', key=key, search_query=search_query)
 
 if __name__ == '__main__':
+    # Create a Database
+    db.create_all()
+    # Run the application
     app.run(debug=True)
